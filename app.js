a9434466c8b5efaa9f84638a221bbfd8a41a8b08
Pierre-Louis Mercereau <pilou@MBPZ35.local>
2017-09-18 07:03:02 +0200

    attempt to extend the schema

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ var apos = require('apostrophe')({
       // uri: 'mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/mydb'
       uri: 'mongodb://mongodb:27017/apostrophe-ehealth'
     },
-    'link-widgets': {}
+    'link-widgets': {},
+    'tool': {},
+    'tool-widgets': {
+      extend: 'apostrophe-pieces-widgets',
+      filters: {
+        projection: {
+          slug: 1,
+          title: 1,
+          type: 1,
+          tags: 1
+        }
+      }
+    },
+    'technology': {}    
   }
 });
